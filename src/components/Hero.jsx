690624f15eb7939178cc9ef6b9a664939c7c844d
Swayam <swayamkumar226@gmail.com>
2025-11-09 19:55:25 +0530

    ux fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,8 @@ import Slate from "./Slate";
 
 export default function Hero() {
   return (
-    <div className=" flex justify-center items-center w-screen h-screen bg-blue-950">
+    <div className=" flex flex-col justify-center items-center w-screen h-screen bg-blue-950 gap-10">
+      <h1 className="text-white text-5xl font-semibold">Tenzes!</h1>
       <Slate />
     </div>
   );
--- src/components/Slate.jsx
+++ src/components/Slate.jsx
@@ -50,10 +50,15 @@ export default function Slate() {
 
   return (
     <div className="flex flex-col items-center justify-center rounded-4xl w-[70%]  h-[80%] bg-white">
-      <section className="w-full h-105 grid grid-cols-5 grid-rows-2 place-items-center gap-8 pl-20 pr-20 pb-10">
+      <p className="text-gray-500 w-95 text-center pt-7 text-md">
+        <span className="font-bold">How to Play:</span> Roll until all dice are
+        the same. Click each die to freeze it at its current value between
+        rolls.
+      </p>
+      <section className="w-full h-105 grid grid-cols-5 grid-rows-2 place-items-center pl-20 pr-20 pb-12">
         {dieElements}
       </section>
-      <section className=" h-20 w-150 flex justify-center items-center">
+      <section className=" h-20 w-150 flex justify-center items-center pb-18">
         <button
           onClick={rollDice}
           className="rounded-xl w-35 h-15 hover:shadow-2xl hover:scale-105 transition transform bg-blue-500 text-xl font-semibold text-white"
