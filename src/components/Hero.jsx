7e2eeb3ae2801d36c629fab31de9eda6d2520da2
Swayam <swayamkumar226@gmail.com>
2026-01-09 19:28:23 +0530

    viewing restrictions for smaller screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,17 @@ import Slate from "./Slate";
 
 export default function Hero() {
   return (
-    <div className=" flex flex-col justify-center items-center w-screen h-screen bg-blue-950 gap-10">
-      <h1 className="text-white text-5xl font-semibold">Tenzies!</h1>
-      <Slate />
-    </div>
+    <>
+      {/* for mobile and small screens*/}
+      <div className="flex lg:hidden w-screen h-screen items-center justify-center bg-blue-900 px-6">
+        <p className="text-white text-xl font-semibold text-center">
+          Please visit with a desktop for a better viewing experience : /
+        </p>
+      </div>
+      <div className=" flex flex-col justify-center items-center w-screen h-screen bg-blue-950 gap-10">
+        <h1 className="text-white text-5xl font-semibold">Tenzies!</h1>
+        <Slate />
+      </div>
+    </>
   );
 }
