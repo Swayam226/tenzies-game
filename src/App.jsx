925f7a06d7407936104ecc876a0da08d514b7407
Swayam <swayamkumar226@gmail.com>
2026-01-13 11:25:00 +0530

    analytics added in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import Hero from "./components/Hero";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
